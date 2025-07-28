d7f94a85ce3188cb549c9e63e2d63b5ad2264d1a
zh1peng <frueddy@gmail.com>
2025-07-28 10:41:56 +0800

    add lang

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ trigger_sender = TriggerSender(
 win, kb = initialize_exp(settings)
 # 6. Setup stimulus bank
 stim_bank = StimBank(win,cfg['stim_config'])\
-    .convert_to_voice('instruction_text')\
+    .convert_to_voice('instruction_text', voice=settings.voice_name)\
     .preload_all()
 
 trigger_sender.send(settings.triggers.get("exp_onset"))
@@ -85,6 +85,3 @@ df.to_csv(settings.res_file, index=False)
 # 10. Close everything
 ser.close()
 core.quit()
-
-
-
